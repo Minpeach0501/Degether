1271004af05d1b7babb0eee3f3bf869ecd1b2796
Minsu <bbodd2013@naver.com>
2022-06-27 18:09:14 +0900

    초기 빌드파일 업로드

---
+++ src/main/java/com/hanghae/degether/DegetherApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.degether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DegetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DegetherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanghae/degether/DegetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.degether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DegetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
