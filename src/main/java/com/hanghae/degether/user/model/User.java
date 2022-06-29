6c3a7fc1a847f77bbc765dd9de36360ae0e48f69
eank0108 <eank0108@naver.com>
2022-06-29 12:22:34 +0900

    docs. project branch 에 master merge

--- src/main/java/com/hanghae/degether/user/model/User.java
+++ src/main/java/com/hanghae/degether/user/model/User.java
@@ -1,16 +1,53 @@
-package com.hanghae.degether.user.model;
+package com.hanghae.degether.User.model;
+
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+
+@Entity(name = "UserInfo")
 @Getter
-@Entity(name = "users")
+@Setter
+@NoArgsConstructor
 public class User {
-    @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long Id;
 
+    @Column
+    private  String username;
+
+    @Column
+    private  String nickname;
+
+    @Column
+    private  String password;
+
+    @Column
+    private  String profileUrl;
+
+    @Column
+    private  String role;
+
+    @Column
+    private  String github;
+
+    @Column
+    private String figma;
+
+    @Column
+    private String intro;
+
+    @Column
+    private boolean status =true ;
+
+
+    public User(String username, String nickname, String profileUrl, String password) {
+        this.username = username;
+        this.nickname = nickname;
+        this.profileUrl = profileUrl;
+        this.password = password;
+    }
 }
