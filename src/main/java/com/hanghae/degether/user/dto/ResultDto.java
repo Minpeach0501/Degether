0f31fa685de9f73baf1b8cf2f593a13bed5a553a
kshee9 <kiang12@naver.com>
2022-07-08 16:28:39 +0900

    fix. 로그인 기능 보완 feat. 프로필 보기 기능 api 추가

---
+++ src/main/java/com/hanghae/degether/user/dto/ResultDto.java
@@ -0,0 +1,34 @@
+package com.hanghae.degether.user.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class ResultDto {
+    private String profileUrl;
+    private String role;
+    private String nickname ;
+    private List<String> language;
+    private String github;
+    private String figma;
+    private String intro;
+
+    private List<ZzimResDto> zzim;
+
+    private List<MyProjectResDto> myProject;
+
+    public ResultDto(String profileUrl, String role, String nickname, List<String> languages, String github, String figma, String intro, List<ZzimResDto> zzim, List<MyProjectResDto> myproject) {
+        this.profileUrl =profileUrl;
+        this.role= role;
+        this.nickname = nickname;
+        this.language = languages;
+        this.github = github;
+        this.figma = figma;
+        this.intro = intro;
+        this.zzim = zzim;
+        this.myProject = myproject;
+    }
+}
