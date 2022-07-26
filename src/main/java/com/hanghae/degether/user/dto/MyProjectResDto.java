3f16ac5c4f3e3c36c83e6625a6d3708df1d21446
eank0108 <eank0108@naver.com>
2022-07-26 22:33:30 +0900

    feat. UserInfo의 myproject 요청시 개발자, 디자이너 카운트

--- src/main/java/com/hanghae/degether/user/dto/MyProjectResDto.java
+++ src/main/java/com/hanghae/degether/user/dto/MyProjectResDto.java
@@ -2,6 +2,7 @@ package com.hanghae.degether.user.dto;
 
 import com.hanghae.degether.project.model.Genre;
 import com.hanghae.degether.project.model.Language;
+import com.hanghae.degether.project.model.Project;
 import com.hanghae.degether.project.model.UserProject;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -31,9 +32,23 @@ public class MyProjectResDto{
 
     String Step;
 
+    Integer devCount;
+    Integer deCount;
+
 
 
     public MyProjectResDto(UserProject userProject) {
+        Project project = userProject.getProject();
+        int devCount = 0;
+        int deCount = 0;
+        for (UserProject userProject2 : project.getUserProjects()) {
+            String role = userProject2.getUser().getRole();
+            if ("백엔드 개발자".equals(role) || "프론트엔드 개발자".equals(role)) {
+                devCount++;
+            } else if ("디자이너".equals(role)) {
+                deCount++;
+            }
+        }
         this.Id = userProject.getProject().getId();
         this.projectName = userProject.getProject().getProjectName();
         this.description = userProject.getProject().getProjectDescription();
@@ -41,5 +56,7 @@ public class MyProjectResDto{
         this.language = userProject.getProject().getLanguages().stream().map(Language::getLanguage).collect(Collectors.toList());
         this.genre = userProject.getProject().getGenres().stream().map(Genre::getGenre).collect(Collectors.toList());
         this.Step = userProject.getProject().getStep();
+        this.devCount = devCount;
+        this.deCount = deCount;
     }
 }
