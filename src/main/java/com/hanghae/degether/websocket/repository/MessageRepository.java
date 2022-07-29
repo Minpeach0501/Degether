814033540b72c29276b5a458c961c049370998dd
eank0108 <eank0108@naver.com>
2022-07-29 23:26:07 +0900

    fix. 채팅 정렬 id 순 변경

--- src/main/java/com/hanghae/degether/websocket/repository/MessageRepository.java
+++ src/main/java/com/hanghae/degether/websocket/repository/MessageRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MessageRepository extends JpaRepository<ChatMessage, Long> {
-    List<FindChatMessageDto> findTop10ByProjectIdOrderByCreatedAtDesc(String projectId);
+    List<FindChatMessageDto> findTop10ByProjectIdOrderByIdDesc(String projectId);
+
 
 }
